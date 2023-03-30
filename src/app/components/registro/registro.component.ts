c7e08dccf01c851e08742cef30d77868013349db
Sebastian Orjuela <sebasorjuela@gmail.com>
2023-03-30 00:19:43 -0500

    se agrego la sección de registro

---
+++ src/app/components/registro/registro.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    const BotonRegistro = document.getElementById('Registrate');
+    const BotonInicio = document.getElementById('Iniciasesion');
+    const contenedor = document.getElementById('contenedor');
+
+    BotonRegistro?.addEventListener('click', () => {
+      contenedor?.classList.add("panel-derecho-activado");
+    });
+    BotonInicio?.addEventListener('click', () => {
+      contenedor?.classList.remove("panel-derecho-activado");
+    });
+  }
+
+  navegar() {
+    this.router.navigate(['/home']);
+  }
+
+}
