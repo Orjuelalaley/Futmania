bd523c92644cec2cd32ccf8ba0275d30ced364da
MrDesilvestri <124096886+MrDesilvestri@users.noreply.github.com>
2023-04-20 13:18:55 -0500

    Se agrego toda la documentación

---
+++ src/app/components/documentacion/documentacion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-documentacion',
+    templateUrl: './documentacion.component.html'
+  })
+
+export class DocumentacionComponent {
+    img = './assets/images/Modelo_entidad_relacion.png';
+}
+
+  
