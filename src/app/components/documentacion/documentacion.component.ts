b0e98ed6de8b1ca7740411f2a33c8306d0179584
MrDesilvestri <124096886+MrDesilvestri@users.noreply.github.com>
2023-04-20 15:24:28 -0500

    Last Update
    errores de ortografia no solucionados.

--- src/app/components/documentacion/documentacion.component.ts
+++ src/app/components/documentacion/documentacion.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { Cancha } from '../canchas/canchas.model';
+
 
 @Component({
     selector: 'app-documentacion',
@@ -13,6 +13,23 @@ export class DocumentacionComponent {
     pagNosotros = './assets/images/paginaNosotros.png';
     pagCanchas = './assets/images/paginaCanchas.png';
     pagEquipo = './assets/images/paginaEquipo.png';
+    imgControladorCancha = './assets/images/controladorCancha.jpg';
+    imgControladorComentario = './assets/images/controladorComentario.jpg';
+    imgControladorUsuario = './assets/images/controladorUsuario.jpg';
+    imgControladorReserva = './assets/images/controladorReserva.jpg';
+
+    imgInterfaceCancha = './assets/images/interfaceCancha.jpg';
+    imgInterfaceComentario = './assets/images/interfaceComentario.jpg';
+    imgInterfaceUsuario = './assets/images/interfaceUsuario.jpg';
+    imgInterfaceReserva = './assets/images/interfaceReserva.jpg';
+
+    imgEntidadCancha = './assets/images/entidadCancha.jpg';
+    imgEntidadComentario = './assets/images/entidadComentario.jpg';
+    imgEntidadUsuario = './assets/images/entidadUsuario.jpg';
+    imgEntidadReserva = './assets/images/entidadReserva.jpg';
+
+    imgMySQL = './assets/images/FotoTablaBase.jpg';
+    imgPruebaUnitaria = './assets/images/pruebaUnitaria.jpg';
 }
 
   
