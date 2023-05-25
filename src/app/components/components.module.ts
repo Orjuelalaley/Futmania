545d57b0900e2c6837f3e8d87f7729265548cd0a
MrDesilvestri <124096886+MrDesilvestri@users.noreply.github.com>
2023-05-25 10:50:56 -0500

    Auth Components, interfaces, services with jwt

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -7,6 +7,8 @@ import { RegistroComponent } from './registro/registro.component';
 import { RegistrarCanchaComponent } from './canchaR/registrar-cancha/registrar-cancha.component';
 import { SolicitarCanchaComponent } from './canchasS/solicitar-cancha/solicitar-cancha.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     RegistroComponent,
     RegistrarCanchaComponent,
     SolicitarCanchaComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserAnimationsModule,
